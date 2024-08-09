6152898cf5508c0988d639e1f4c4fd50406b50e6
jotzumi <jotzumi@gmail.com>
2024-08-09 15:23:57 +0300

    Update index.vue

--- src/modules/[chain]/params/index.vue
+++ src/modules/[chain]/params/index.vue
@@ -38,11 +38,11 @@ onMounted(() => {
     <CardParameter :cardItem="store.distribution" />
     <!-- Slashing Parameters -->
     <CardParameter :cardItem="store.slashing" />
-    <!-- Application Version -->
+    <!-- Application Version 
     <div class="bg-base-100 px-4 pt-3 pb-4 rounded-sm mt-6">
       <div class="text-base mb-3 text-main">{{ store.appVersion?.title }}</div>
       <ArrayObjectElement :value="store.appVersion?.items" :thead="false" />
-    </div>
+    </div> -->
 
     <!-- Node Information 
     <div class="bg-base-100 px-4 pt-3 pb-4 rounded-sm mt-6">
