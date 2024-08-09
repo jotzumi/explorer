c17b925b4521470963e57e48c2b7a043870e80a4
jotzumi <jotzumi@gmail.com>
2024-08-09 16:27:56 +0300

    Update index.vue

--- src/modules/[chain]/index.vue
+++ src/modules/[chain]/index.vue
@@ -382,11 +382,11 @@ const amount = computed({
       </Teleport>
     </div>
 
-    <!--<div class="bg-base-100 rounded mt-4"> -->
-    <!--  <div class="px-4 pt-4 pb-2 text-lg font-semibold text-main"> 
+    <!--<div class="bg-base-100 rounded mt-4">
+      <div class="px-4 pt-4 pb-2 text-lg font-semibold text-main"> 
         {{ $t('index.app_versions') }}
-      </div> -->
-      <!-- Application Version 
+      </div>
+       Application Version 
       <ArrayObjectElement :value="paramStore.appVersion?.items" :thead="false" />
       <div class="h-4"></div> 
     </div>-->
